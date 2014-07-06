09f7cffc5bdc182934289c51b2ae08fdb9054b9a
Allen Riddell <abr@ariddell.org>
2014-07-06 08:22:19 -0400

    Use re.findall instead of re.split to leave text unmodified
    Ideally one should be able to trivially recover the original text by using
    ''.join(parts) on the respective parts.

--- splitter/splitter.py
+++ splitter/splitter.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Split a single text into smaller parts.
 
 Counts words by counting spaces. Optionally, the script will attempt to preserve
@@ -11,41 +12,82 @@ import logging
 import math
 import os
 import re
-import string
 
 logger = logging.getLogger('splitter')
 
+_SENT_RE = re.compile(r'[^\.。]*[\.。]\s*')
+_TOKEN_RE = re.compile(r'\s*\S+\s*')
+
 
 def _tokenize(s):
-    return re.split(r'\s+', s)
+    """Tokenize string without capturing any whitespace."""
+    return _TOKEN_RE.findall(s)
 
 
-def _split(filename, n_words, preserve_sentences):
-    """Split a long text into chunks of approximately `n_words` words."""
-    with open(filename, 'r') as input:
-        words = _tokenize(input.read())
+def _sent_tokenize(s):
+    """Split string into sentences delimited by periods.
+
+    Delimiters are not captured. Matches CJK period as well as ASCII period.
+    """
+    return _SENT_RE.findall(s)
+
+
+def _split_preserve_sentences(text, n_words):
+    """Split a long text into chunks of approximately `n_words` words.
+
+    Attempt to preserve sentences.
+    """
+    sentences = _sent_tokenize(text)
+    # we want to treat the list of sentences as a stack that we can pop from
+    # and push to. The easiest way to do this is just to reverse the list and
+    # use the methods pop and append.
     chunks = []
     current_chunk_words = []
-    current_chunk_word_count = 0
-    for word in words:
-        current_chunk_words.append(word)
-        if word not in string.whitespace:
-            current_chunk_word_count += 1
-        if current_chunk_word_count == n_words:
-            chunk = ' '.join(current_chunk_words)
+    sentences.reverse()
+    while sentences:
+        current_chunk_words_copy = current_chunk_words.copy()
+        sent = sentences.pop()
+        words = _tokenize(sent)
+        current_chunk_words.extend(words)
+        if len(words) > n_words:
+            raise RuntimeError("One sentence contains more than `n_words`: {}".format(sent))
+        if len(current_chunk_words) > n_words:
+            # over limit, push current sentence back onto stack
+            sentences.append(sent)
+            chunk = ''.join(current_chunk_words_copy)
             chunks.append(chunk)
             # start over for the next chunk
             current_chunk_words = []
-            current_chunk_word_count = 0
-    final_chunk = ' '.join(current_chunk_words)
+    final_chunk = ''.join(current_chunk_words)
+    chunks.append(final_chunk)
+    return chunks
+
+
+def _split(text, n_words):
+    """Split a long text into chunks of approximately `n_words` words."""
+    words = _tokenize(text)
+    chunks = []
+    current_chunk_words = []
+    for word in words:
+        current_chunk_words.append(word)
+        if len(current_chunk_words) == n_words:
+            chunk = ''.join(current_chunk_words)
+            chunks.append(chunk)
+            # start over for the next chunk
+            current_chunk_words = []
+    final_chunk = ''.join(current_chunk_words)
     chunks.append(final_chunk)
     return chunks
 
 
 def splitter(filename, output_dir, n_words, preserve_sentences):
     """Splits text and writes parts to files."""
+    text = open(filename, 'r').read()
     filename_base, filename_ext = os.path.splitext(os.path.basename(filename))
-    chunks = _split(filename, n_words, preserve_sentences)
+    if preserve_sentences:
+        chunks = _split_preserve_sentences(text, n_words)
+    else:
+        chunks = _split(text, n_words)
 
     # we want a suffix that identifies the chunk, such as "02" for the 2nd
     # chunk. Python has a couple of standard ways of doing this that should
